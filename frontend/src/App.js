4c29e3271c7019717883366f5d1ee81f55430b52
RyuHyoKyung <rhyo0410@naver.com>
2021-06-03 17:24:58 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router';
+
+const App = () => {
+  return (<>
+  <nav>
+   <Link to="/">Home</Link>
+   <Link to="/">Blog</Link>
+   <Link to="/">About Me</Link>
+  </nav>
+  <h1>첫번째 크기 헤드라인</h1>
+  <h2>두 번째 크기 헤드라인</h2>
+  <h3>세 번째 크기 헤드라인</h3>
+  <h4>네 번째 크기 헤드라인</h4>
+  <h5>다섯 번째 크기 헤드라인</h5>
+  <p>문단은 p로 쓰세요. p는 아마도 Paragraph의 앞글자를 따온 것이겠죠?</p>
+  <Link to="">Go to google</Link>
+  <hr/>
+  <img src="/" width="600px"></img>
+  </>)
+}
+
+export default App;
