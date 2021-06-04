06da24935c883733c59da4350c07d4f1792621be
RyuHyoKyung <rhyo0410@naver.com>
2021-06-04 14:50:30 +0900

    save before commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Signup from './user/SignUp'
+import reportWebVitals from'.reportWebBitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Signup/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/user/UserDetail.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UserDetail = () =>{
+    return(<></>)
+
+}
+
+export default UserDetail
